ca5c1a937d0d92ef396e24d81de35dc888ea9d0c
Yu-Hsiang-Hunag <75356419+Yu-Hsiang-Hunag@users.noreply.github.com>
2022-10-24 17:22:57 +0800

    Add files via upload

--- Helloworld.py
+++ Helloworld.py
@@ -1,9 +1,21 @@
-import argparse
-import logging
-import xml.etree.ElementTree as ET # read xml
+try:
+    import argparse
+except ImportError:
+    print("Please install argparse")
+try:
+    import logging
+except ImportError:
+    print("Please install logging")
+try:
+    import xml.etree.ElementTree as ET # read xml
+except ImportError:
+    print("Please install xml.etree.ElementTree")
 import os
 import re
-from unidiff import PatchSet # module for getting diff file information
+try:
+    from unidiff import PatchSet # module for getting diff file information
+except:
+    print("Please install unidiff")
 # win32com --> open excel
 # try:
 #     import win32com.client
@@ -18,3 +30,15 @@ from common import oempri
 
 print("Import Success")
 print("Hello World")
+
+# Parser Function # This needed to modify based on the requirement
+parser = argparse.ArgumentParser(description='pri_compare', formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument('-u', '--account', metavar="</str>", required=True)
+parser.add_argument('-p', '--password', metavar="</str>", required=True)
+parser.add_argument('-t', '--jira_ticket', metavar="</str>", required=True)
+
+args = parser.parse_args()
+print(args.account)
+print(args.password)
+print(args.jira_ticket)
