32889377037e9224dfeefc0a8d1595f88b9824ee
Yu-Hsiang-Hunag <75356419+Yu-Hsiang-Hunag@users.noreply.github.com>
2022-10-24 14:45:44 +0800

    Add files via upload

--- Helloworld.py
+++ Helloworld.py
@@ -5,16 +5,16 @@ import os
 import re
 from unidiff import PatchSet # module for getting diff file information
 # win32com --> open excel
-# try:
-#     import win32com.client
-# except ImportError:
-#     logging.error("Please Install win32com")
-#     raise ImportError
+try:
+    import win32com.client
+except ImportError:
+    logging.error("Please Install win32com")
+    raise ImportError
 
 # custom libraries #
-# from common import excel
+from common import excel
 from common import oempri
-# from common import common
+from common import common
 
 print("Import Success")
 print("Hello World")
